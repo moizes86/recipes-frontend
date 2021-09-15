d99d1e9ed1ca6e1b00af20c6e4da5008c4a00a12
moizes86 <moshe.mn86@gmail.com>
2021-09-15 15:17:28 +0300

    Fixed origin

--- src/services/http_Service.js
+++ src/services/http_Service.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 
+const origin =
+  process.env.NODE_ENV === "development" ? "http://localhost:3100" : "https://recipes-db-mm.herokuapp.com";
+
 export const httpService = (method, url, data) => {
   return axios({
     method: method,
     url: url,
     data,
-    headers: { "Access-Control-Allow-Origin": "http://localhost:3100" },
+    headers: { "Access-Control-Allow-Origin": origin },
     withCredentials: true,
   });
 };
